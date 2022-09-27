fe057b5d8ec723b3e28082ed3dd855128e985f7a
ngran4 <nicgranville@gmail.com>
2022-09-27 14:23:25 -0700

    photo schema complete, update mapItemSchema

--- models/location.js
+++ models/location.js
@@ -1,12 +1,38 @@
 const mongoose = require('mongoose');
 
-const mapItemSchema = new mongoose.Schema({
-  name: String,
-  location: {
-    type: {type: String, default: 'Point'},
-    coordinates: {type: [Number], default: [0, 0]}
-  }
 
+const photoSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  photoUrl: String,
+})
+
+const mapItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a location'],
+    unique: true,
+    maxlength: [75],
+  },
+  address: {
+    type: string,
+    required: [true, 'Please add an address']
+  },
+  location: {
+    type: {
+      type: String, 
+      enum: ['Point'],
+    },
+    coordinates: {
+      type: [Number], 
+      index: '2dsphere' // supports queries that calculate geometries on an earth-like sphere 
+    }, 
+    formattedAddress: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  photos: [photoSchema]
 });
 
 const MapItem = mongoose.model('MapItem', mapItemSchema);
@@ -22,3 +48,4 @@ module.exports = MapItem;
 //   accessibility: String,
 //   photoUrl: String,
 // });
+
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import mapboxgl from 'mapbox-gl';
+
+export default function Map(){
+
+  return(
+    <h1> map </h1>
+  )
+}
